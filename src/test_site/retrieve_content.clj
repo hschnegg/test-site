5f0068195186484e27101654ca6cc98ad5b9d2b6
hschnegg <hschnegg@gmail.com>
2017-06-25 12:10:46 +0100

    Initial tools to pull content from Gurdian open API

--- src/test_site/retrieve_content.clj
+++ src/test_site/retrieve_content.clj
@@ -8,18 +8,53 @@
             [cheshire.core :refer :all]))
 
 
-
-(def search {"from-date" "2017-05-19"})
+(def api-key {"api-key" (:api-key (:guardian-api env))})
+(def search {"tag" "technology/artificialintelligenceai" "from-date" "2017-05-01"})
 (def show {"Show-blocks" "body", "show-tags" "all"})
 
-(def payload (conj search {"api-key" (:api-key (:guardian-api env))}))
+(def query-params (conj search api-key show))
 
 (def api-return (client/get (:url (:guardian-api env))
-                            {:headers payload}))
+                            {:query-params query-params}))
 
 (def response ((parse-string (api-return :body)) "response"))
 
-(response "pageSize")
-(response "pages")
+(def page-size (response "pageSize"))
+(def pages (response "pages"))
 
 
+(def article-count 15)
+
+(def required-pages (min pages (Math/ceil (/ article-count page-size))))
+
+
+(defn query-guardian-api [tag from-date & [page show]]
+  (let [search {"tag" tag "from-date" from-date}
+        page {"page" (if page page 1)}
+        show {"show-blocks" (if show show "")}
+        api-key {"api-key" (:api-key (:guardian-api env))}
+        url (:url (:guardian-api env))
+        query-params (conj search page show api-key)
+        api-return (client/get url
+                               {:query-params query-params})
+        api-return-body (parse-string (api-return :body))
+        response (api-return-body "response")]
+    response))
+  
+
+(defn check-available-content [tag from-date]
+  (let [response (query-guardian-api tag from-date)
+        available-content {:pages (response "pages")
+                           :page-size (response "pageSize")}]
+    available-content))
+
+
+(def r (check-available-content "sport/sport" "2017-06-01"))
+
+
+(defn retrieve-page [page tag from-date]
+  (let [response (query-guardian-api tag from-date page "body")]
+    response))
+    
+
+(def res (retrieve-page 1 "technology/artificialintelligenceai" "2017-06-01"))
